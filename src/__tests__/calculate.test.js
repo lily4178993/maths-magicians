6bec5dd13574398d9d033883bef939e2e7f0afa6
lily4178993 <nellytelli@gmail.com>
2023-09-21 18:55:35 +0000

    Changes: Create unit test for calulator logic functions

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,1 +1,34 @@
-// Tests for calculate.js file
+/* import calculate from '../logic/calculate';
+
+describe('calculate function', () => {
+  it('should handle AC (All Clear) button', () => {
+    const result = calculate({ total: '5', next: '3', operation: '+' }, 'AC');
+    expect(result).toEqual({ total: null, next: null, operation: null });
+  });
+
+  it('should handle number input', () => {
+    const result = calculate({ total: '5', next: '3', operation: '+' }, '7');
+    expect(result).toEqual({ total: '5', next: '37', operation: '+' });
+  });
+
+  it('should handle consecutive zeroes', () => {
+    const result = calculate({ total: '5', next: '0', operation: '+' }, '0');
+    expect(result).toEqual({});
+  });
+
+  it('should handle decimal input', () => {
+    const result = calculate({ total: '5', next: '3', operation: '+' }, '.');
+    expect(result).toEqual({ total: '5', next: '3.', operation: '+' });
+  });
+
+  it('should handle = (equals) button', () => {
+    const result = calculate({ total: '5', next: '3', operation: '+' }, '=');
+    expect(result).toEqual({ total: '8', next: null, operation: null });
+  });
+
+  it('should handle +/- button', () => {
+    const result = calculate({ total: '5', next: '3', operation: '+' }, '+/-');
+    expect(result).toEqual({ total: '5', next: '-3', operation: '+' });
+  });
+});
+ */
--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,1 +1,38 @@
-// Tests for operate.js file
+/* import operate from '../logic/operate';
+
+describe('operate function', () => {
+  it('should add two numbers', () => {
+    expect(operate('5', '3', '+')).toBe('8');
+  });
+
+  it('should subtract two numbers', () => {
+    expect(operate('5', '3', '-')).toBe('2');
+  });
+
+  it('should multiply two numbers', () => {
+    expect(operate('5', '3', 'x')).toBe('15');
+  });
+
+  it('should divide two numbers', () => {
+    expect(operate('6', '2', '÷')).toBe('3');
+  });
+
+  it('should handle division by zero', () => {
+    expect(operate('6', '0', '÷')).toBe("Can't divide by 0.");
+  });
+
+  it('should find modulo of two numbers', () => {
+    expect(operate('7', '3', '%')).toBe('1');
+  });
+
+  it('should handle modulo when dividing by zero', () => {
+    expect(operate('7', '0', '%')).toBe(
+      "Can't find modulo as can't divide by 0.",
+    );
+  });
+
+  it('should throw an error for an unknown operation', () => {
+    expect(() => operate('5', '3', '^')).toThrow("Unknown operation '^'");
+  });
+});
+ */
